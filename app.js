a1af85e971f1eabcd25f3146d3218f3493bdd781
babosaji123 <151328131+babosaji123@users.noreply.github.com>
2026-09-01 18:27:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.use(limiter)
 app.use(cors({
 	origin:["http://localhost:5173","https://encryptsharee.netlify.app"],
   credentials:true,
+	 methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Password", "password"],
   exposedHeaders: ['Content-Disposition']
 }));
 app.use(fileUpload());
