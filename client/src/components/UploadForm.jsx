4003df8d753b6ce160a4ef5f68770989e1ba23ef
babosaji123 <151328131+babosaji123@users.noreply.github.com>
2026-09-01 18:26:30 +0530

    Update UploadForm.jsx

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -147,7 +147,7 @@ const UploadForm = ({ progress }) => {
       formData.append("password", hashedPassword);
 
 
-      await axios.post("https://encryptshare.onrender.com", formData, {
+     const res= await axios.post("https://encryptshare.onrender.com", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
